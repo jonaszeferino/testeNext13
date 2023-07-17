ed43db5ea815dee494b80a5028e85e2de54e3929
jonaszeferino <jonaszeferino@gmail.com>
2023-07-17 00:08:52 -0300

    first commit

---
+++ app/pageTeste2/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+    return (
+     
+  
+      <>Page Teste 2</>
+    )
+  }
+  
+
+  // agora no NEXT.13 o caminho das paginas se da pela pasta, e dentro dela sempre teremos a págian page.tsx e a pag page.tsx na raiz da pasta app seria equivalente a página index.js ou a home /
+  
