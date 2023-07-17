b5f9b3d6d819f00daaf9c6015f08c78e8bee2a04
jonaszeferino <jonaszeferino@gmail.com>
2023-07-17 14:23:37 -0300

    new project next.js 13 with tailwind new link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -45,9 +45,9 @@ export default function Navbar() {
   
                 <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                   
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-0">Perfil</a>
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-1">Configurações</a>
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-2">Deslogar</a>
+                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-0">Perfil</a>
+                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem"  id="user-menu-item-1">Configurações</a>
+                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem"  id="user-menu-item-2">Deslogar</a>
                 </div>
               </div>
             </div>
