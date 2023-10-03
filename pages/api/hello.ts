088bf565afe1c6dceb54d6e76c7b30f59c1b2d35
jonaszeferino <jonaszeferino@gmail.com>
2023-10-03 00:41:18 -0300

    first commit

--- pages/api/hello.ts
+++
@@ -1,5 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ message: 'Hello This Is My Next.js API' });
-}
